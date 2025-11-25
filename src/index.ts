91d9d227328a8405d7186baddaef2645faa49a9b
Aminulhoque01 <aminuhoque53@gmail.com>
2025-11-25 16:51:42 +0600

    backend project setup

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
+import errorHandler from "./utils/errorHandler";
+
+ 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Health check route
+app.get("/health", (_req, res) => {
+  res.status(200).json({ status: "Antopolis-UP" });
+});
+
+app.use(errorHandler);
+
+// app.use("/api/v1/generate", legalRoutes);
+
+// MongoDB connection
+const connectDB = async () => {
+  console.log("🔄 Attempting MongoDB connection...");
+  
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("✅ MongoDB Connected");
+    console.log("🚀 Database connected successfully");
+  } catch (err) {
+    console.error("❌ DB Connection Failed:", err);
+    process.exit(1);
+  }
+};
+
+// Start server only after DB connection
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀Antopolis-backend Server running on port ${PORT}`);
+  });
+});
+
